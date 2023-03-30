ae89e636ea3af263d9c31268e23fa47bfe6d31e7
Javi <javiermespino@gmail.com>
2023-03-30 14:14:17 +0000

    Test(red): check that there are 7 ice creams when Pedro takes ice creams

--- src/test/kotlin/MaxibonTest.kt
+++ src/test/kotlin/MaxibonTest.kt
@@ -10,4 +10,9 @@ class MaxibonTest {
     fun `check that there are 10 ice creams when started`() {
         assertEquals(testMaxibon.getMaxibons(), 10)
     }
+
+    @Test
+    fun `check that there are 7 ice creams when Pedro takes ice creams`() {
+        assertEquals(testMaxibon.getMaxibons(), 7)
+    }
 }
