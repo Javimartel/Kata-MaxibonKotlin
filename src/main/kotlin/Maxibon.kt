e50c78fccc6993de22e850081694bffe839a75c6
Javi <javiermespino@gmail.com>
2023-03-30 14:30:17 +0000

    Test(refactor): check that there are 9 ice creams when Fran takes ice creams

--- src/main/kotlin/Maxibon.kt
+++ src/main/kotlin/Maxibon.kt
@@ -2,11 +2,9 @@ class Maxibon {
     private var maxibons = 10
 
     fun takeMaxibon(developer: String) {
-        if(developer === "Pedro") {
-            this.maxibons -= 3
-        }
-        if(developer === "Fran") {
-            this.maxibons -= 1
+        when (developer) {
+            "Pedro" -> this.maxibons -= 3
+            "Fran" -> this.maxibons -= 1
         }
     }
 
