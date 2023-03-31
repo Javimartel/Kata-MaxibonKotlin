ef9dfbed0b9ab81b16d718b58d1279918ebd987d
Javi <javiermespino@gmail.com>
2023-03-31 10:41:57 +0000

    Test(green): throw an Exception when ice creams are 2 and then there are 12

--- src/main/kotlin/Maxibon.kt
+++ src/main/kotlin/Maxibon.kt
@@ -7,6 +7,10 @@ class Maxibon {
             "Fran", "Jorge" -> this.maxibons -= 1
             "Sergio" -> this.maxibons -= 2
         }
+        if (this.maxibons <= 2) {
+            this.maxibons += 10
+            throw MaxibonLimitException("Hi guys, I'm $developer. We need more maxibons!")
+        }
     }
 
     fun getMaxibons(): Int {
